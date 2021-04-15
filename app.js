ff26f97cd52a75d6d56381ec0eb0a21b879d38c9
Bob Farrell <bobfarrell@Bobs-MBP.attlocal.net>
2021-04-15 17:12:45 -0500

    Updated project files

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+var id;
+
+function allowDrop(event)
+{
+    event.preventDefault();
+}
+
+function dragStart(event)
+{
+    id=event.target.id;
+}
+
+function drop(event)
+{
+    event.target.append(document.getElementById(id));
+}
